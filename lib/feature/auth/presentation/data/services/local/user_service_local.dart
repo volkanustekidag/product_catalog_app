83014abca509a43494b2cb5086dd09822e345264
volkanustekidag <volkanustekidg@gmail.com>
2022-11-04 12:27:24 +0300

    Create user local service

---
+++ lib/feature/auth/presentation/data/services/local/user_service_local.dart
@@ -0,0 +1,9 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserServiceLocal {
+  Future saveToken(token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("token", token);
+    print("a---------------${prefs.getString("token")}");
+  }
+}
