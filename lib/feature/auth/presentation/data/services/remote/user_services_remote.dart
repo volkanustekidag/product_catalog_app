e1db5578c1abb7992dc83df55692f88c6a301f73
volkanustekidag <volkanustekidg@gmail.com>
2022-11-04 12:27:11 +0300

    Create user remote service

---
+++ lib/feature/auth/presentation/data/services/remote/user_services_remote.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'dart:io';
+
+import "package:http/http.dart" as http;
+import 'package:product_catalog_app/feature/auth/presentation/data/dto/user_model.dart';
+
+class UserServicesRemote {
+  Future<dynamic> post(Map<String, dynamic> body, url) async {
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: body,
+        headers: {
+          'Accept': 'application/json',
+        },
+      );
+
+      switch (response.statusCode) {
+        case HttpStatus.ok:
+          final jsonBody = jsonDecode(response.body);
+          return UserModel.fromJson(jsonBody).toEntity();
+        default:
+          throw response.body;
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+}
