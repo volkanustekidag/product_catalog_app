e368c23725385ddd2fee69465259344da55c5337
volkanustekidag <volkanustekidg@gmail.com>
2022-11-04 12:32:09 +0300

    Add controller and button in login & register view

--- lib/feature/auth/presentation/widgets/login.dart
+++ lib/feature/auth/presentation/widgets/login.dart
@@ -1,29 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:product_catalog_app/feature/auth/presentation/data/repositories/user_repositories.dart';
+import 'package:product_catalog_app/feature/auth/presentation/provider/check_box_provider.dart';
 import 'package:product_catalog_app/feature/auth/presentation/utils/validations.dart';
+import 'package:product_catalog_app/feature/auth/presentation/widgets/auth_button.dart';
 import 'package:product_catalog_app/feature/auth/presentation/widgets/remember_me_check_box.dart';
 import 'package:product_catalog_app/feature/auth/presentation/widgets/textfieldform.dart';
+import 'package:provider/provider.dart';
 
 class Login extends StatelessWidget {
-  const Login({
+  Login({
     Key? key,
   }) : super(key: key);
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController textEditingControllerEmail =
+      TextEditingController();
+  final TextEditingController textEditingControllerPassword =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          TextFieldForm(
-            label: "E-mail",
-            validation: Validations().validationEmail,
-          ),
-          TextFieldForm(
-            label: "Password",
-            validation: Validations().validationPassword,
-          ),
-          RememberMeCheckBox()
-        ],
+    print(context.widget.toString());
+
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFieldForm(
+              label: "E-mail",
+              validation: Validations().validationEmail,
+              textEditingController: textEditingControllerEmail,
+            ),
+            TextFieldForm(
+              label: "Password",
+              validation: Validations().validationPassword,
+              textEditingController: textEditingControllerPassword,
+            ),
+            const RememberMeCheckBox(),
+            AuthButton(
+              title: "Login",
+              onPressed: () => _formKey.currentState!.validate()
+                  ? UserRepository(context: context).userLogin(
+                      textEditingControllerEmail.value.text,
+                      textEditingControllerPassword.value.text,
+                      Provider.of<CheckBoxProvider>(context, listen: false)
+                          .rememberMe,
+                    )
+                  : null,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/feature/auth/presentation/widgets/register.dart
+++ lib/feature/auth/presentation/widgets/register.dart
@@ -1,50 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:product_catalog_app/feature/auth/presentation/data/repositories/user_repositories.dart';
 import 'package:product_catalog_app/feature/auth/presentation/utils/validations.dart';
+import 'package:product_catalog_app/feature/auth/presentation/widgets/auth_button.dart';
 import 'package:product_catalog_app/feature/auth/presentation/widgets/textfieldform.dart';
 
 class Register extends StatelessWidget {
-  const Register({
+  Register({
     Key? key,
   }) : super(key: key);
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController textEditingControllerEmail =
+      TextEditingController();
+  final TextEditingController textEditingControllerPassword =
+      TextEditingController();
+  final TextEditingController textEditingControllerName =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          TextFieldForm(
-            label: "Name",
-            validation: Validations().validationValue,
-          ),
-          TextFieldForm(
-            label: "Surname",
-            validation: Validations().validationValue,
-          ),
-          TextFieldForm(
-            label: "Phone Number",
-            textInputType: TextInputType.number,
-            inputFormatters: [
-              MaskTextInputFormatter(
-                  mask: '+9# (###) ###-##-##',
-                  filter: {"#": RegExp(r'[0-9]')},
-                  type: MaskAutoCompletionType.lazy)
-            ],
-          ),
-          TextFieldForm(
-            label: "E-mail",
-            validation: Validations().validationEmail,
-          ),
-          TextFieldForm(
-            label: "Password",
-            validation: Validations().validationPassword,
-          ),
-          TextFieldForm(
-            label: "Password",
-            validation: Validations().validationPassword,
-          )
-        ],
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFieldForm(
+              label: "Name",
+              validation: Validations().validationValue,
+              textEditingController: textEditingControllerName,
+            ),
+            TextFieldForm(
+              label: "Surname",
+              validation: Validations().validationValue,
+            ),
+            TextFieldForm(
+              label: "Phone Number",
+              textInputType: TextInputType.number,
+              inputFormatters: [
+                MaskTextInputFormatter(
+                    mask: '+9# (###) ###-##-##',
+                    filter: {"#": RegExp(r'[0-9]')},
+                    type: MaskAutoCompletionType.lazy)
+              ],
+            ),
+            TextFieldForm(
+              label: "E-mail",
+              validation: Validations().validationEmail,
+              textEditingController: textEditingControllerEmail,
+            ),
+            TextFieldForm(
+              label: "Password",
+              validation: Validations().validationPassword,
+              textEditingController: textEditingControllerPassword,
+            ),
+            TextFieldForm(
+              label: "Password",
+              validation: Validations().validationPassword,
+            ),
+            AuthButton(
+              title: "Register",
+              onPressed: () => _formKey.currentState!.validate()
+                  ? UserRepository(context: context).userRegister(
+                      textEditingControllerName.value.text,
+                      textEditingControllerEmail.value.text,
+                      textEditingControllerPassword.value.text)
+                  : null,
+            )
+          ],
+        ),
       ),
     );
   }
