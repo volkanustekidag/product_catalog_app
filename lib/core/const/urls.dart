decde9c1342a2aa81a838e8a030e0971195505c1
volkanustekidag <volkanustekidg@gmail.com>
2022-11-04 12:24:58 +0300

    Add urls file

---
+++ lib/core/const/urls.dart
@@ -0,0 +1,6 @@
+class Urls {
+  static const REGISTER_URL =
+      "https://assignment-api.piton.com.tr/api/v1/user/register";
+  static const LOGIN_URL =
+      "https://assignment-api.piton.com.tr/api/v1/user/login";
+}
