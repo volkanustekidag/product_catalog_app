071ad99b4a7c73a2db4182b67d79873c3d8a1b96
volkanustekidag <volkanustekidg@gmail.com>
2022-11-04 22:50:43 +0300

    Change image and description widget

--- lib/feature/home/presentation/widgets/product_listview.dart
+++ lib/feature/home/presentation/widgets/product_listview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:product_catalog_app/core/widgets/product_image.dart';
 import 'package:product_catalog_app/domain/models/product.dart';
 import 'package:product_catalog_app/feature/home/presentation/provider/product_provider.dart';
 import 'package:product_catalog_app/feature/product_detail/product_detail_screen.dart';
@@ -34,27 +35,7 @@ class ProductListview extends StatelessWidget {
                       Text(
                         "${products?.products?[index].name}",
                       ),
-                      Row(
-                        children: [
-                          Expanded(
-                            flex: 1,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Image.network(
-                                  ("https://assignment-api.piton.com.tr${products!.products?[index].image}")),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Padding(
-                              padding: const EdgeInsets.all(20.0),
-                              child: Text(
-                                "${products?.products?[index].description}",
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                      ProductImage(product: products?.products?[index]),
                       IconButton(
                         icon: Icon(products!.favCount!
                                 .contains(products!.products![index].id)
