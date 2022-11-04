32713c140b20a011a31030f2ae4970dd1033f4db
volkanustekidag <volkanustekidg@gmail.com>
2022-11-04 12:14:20 +0300

    Add text editing controller in textformfield

--- lib/feature/auth/presentation/widgets/textfieldform.dart
+++ lib/feature/auth/presentation/widgets/textfieldform.dart
@@ -5,34 +5,35 @@ class TextFieldForm extends StatelessWidget {
   final validation;
   final inputFormatters;
   final TextInputType? textInputType;
-  const TextFieldForm(
-      {Key? key,
-      this.label,
-      this.validation,
-      this.textInputType,
-      this.inputFormatters})
-      : super(key: key);
+  final TextEditingController? textEditingController;
+  const TextFieldForm({
+    Key? key,
+    this.label,
+    this.validation,
+    this.textInputType,
+    this.inputFormatters,
+    this.textEditingController,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Form(
+      child: TextFormField(
+        controller: textEditingController,
+        validator: validation != null ? (value) => validation(value) : null,
+        keyboardType: textInputType,
         autovalidateMode: AutovalidateMode.onUserInteraction,
-        child: TextFormField(
-          validator: validation != null ? (value) => validation(value) : null,
-          keyboardType: textInputType,
-          inputFormatters: inputFormatters ?? [],
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-            ),
-            label: Text(
-              label!,
-            ),
+        inputFormatters: inputFormatters ?? [],
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          label: Text(
+            label!,
           ),
         ),
       ),
