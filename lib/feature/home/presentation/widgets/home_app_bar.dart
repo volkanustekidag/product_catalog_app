962addd70abe6e4d05947d437c2c9c49f779962a
volkanustekidag <volkanustekidg@gmail.com>
2022-11-04 23:08:11 +0300

    Change colors and added ontap on favorite button

--- lib/feature/home/presentation/widgets/home_app_bar.dart
+++ lib/feature/home/presentation/widgets/home_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:product_catalog_app/feature/home/presentation/provider/product_provider.dart';
+import 'package:product_catalog_app/feature/product_fav_list/product_fav_list.dart';
 import 'package:provider/provider.dart';
 
 class HomeAppBar extends StatelessWidget with PreferredSizeWidget {
@@ -13,51 +14,56 @@ class HomeAppBar extends StatelessWidget with PreferredSizeWidget {
 
     return AppBar(
       centerTitle: true,
-      backgroundColor: Colors.white,
       title: const Text(
         "PRODUCTS",
-        style: TextStyle(color: Colors.black),
       ),
       actions: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SizedBox(
-            width: 40,
-            child: Stack(
-              children: [
-                const Icon(
-                  Icons.favorite,
-                  size: 30,
-                  color: Colors.black,
-                ),
-                Positioned(
-                  left: 5,
-                  top: 0,
-                  child: Container(
-                    width: 30,
-                    height: 30,
-                    alignment: Alignment.topRight,
-                    margin: const EdgeInsets.only(top: 5),
+        InkWell(
+          onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ProductFavList(),
+              )),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 40,
+              child: Stack(
+                children: [
+                  const Icon(
+                    Icons.favorite,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  Positioned(
+                    left: 5,
+                    top: 0,
                     child: Container(
-                      width: 18,
-                      height: 18,
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.red,
-                          border: Border.all(color: Colors.white, width: 1)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(0.0),
-                        child: Center(
-                          child: Text(
-                            "${products?.favCount?.length}",
-                            style: const TextStyle(fontSize: 14),
+                      width: 30,
+                      height: 30,
+                      alignment: Alignment.topRight,
+                      margin: const EdgeInsets.only(top: 5),
+                      child: Container(
+                        width: 18,
+                        height: 18,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.red,
+                            border: Border.all(color: Colors.blue, width: 1)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: Center(
+                            child: Text(
+                              "${products?.favCount?.length}",
+                              style: const TextStyle(fontSize: 14),
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         )
