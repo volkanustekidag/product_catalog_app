39bdba8ae2394645f751ab81ba1e37cfc46a6133
volkanustekidag <volkanustekidg@gmail.com>
2022-11-04 22:17:03 +0300

    Add post method

--- lib/core/data/services/product_services.dart
+++ lib/core/data/services/product_services.dart
@@ -3,19 +3,17 @@ import 'dart:io';
 
 import "package:http/http.dart" as http;
 import 'package:product_catalog_app/core/data/dto/all_product_model.dart';
+import 'package:product_catalog_app/core/data/dto/favorite_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ProductServices {
   Future<dynamic> get(url) async {
-    final prefs = await SharedPreferences.getInstance();
-    String? token = prefs.getString("token");
-
     try {
       final response = await http.get(
         Uri.parse(url),
         headers: {
           'Accept': 'application/json',
-          'access-token': token!,
+          'access-token': await getToken(),
         },
       );
 
@@ -28,6 +26,31 @@ class ProductServices {
       }
     } catch (e) {
       return null;
+    }
+  }
+
+  Future<String> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString("token");
+    return token!;
+  }
+
+  Future<dynamic> post(Map<String, dynamic> body, url) async {
+    final response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: {
+        'Accept': 'application/json',
+        'access-token': await getToken(),
+      },
+    );
+    print(response.body);
+    switch (response.statusCode) {
+      case HttpStatus.ok:
+        final jsonBody = jsonDecode(response.body);
+        return FavoriteModel.fromJson(jsonBody);
+      default:
+        throw response.body;
     }
   }
 }
