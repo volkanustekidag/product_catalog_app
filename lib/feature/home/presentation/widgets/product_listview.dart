2f716d4e18b300ed4c75843fd82fd0bad807050a
volkanustekidag <volkanustekidg@gmail.com>
2022-11-04 22:48:36 +0300

    Add inkwell for listview item

--- lib/feature/home/presentation/widgets/product_listview.dart
+++ lib/feature/home/presentation/widgets/product_listview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:product_catalog_app/domain/models/product.dart';
 import 'package:product_catalog_app/feature/home/presentation/provider/product_provider.dart';
+import 'package:product_catalog_app/feature/product_detail/product_detail_screen.dart';
 import 'package:provider/provider.dart';
 
 class ProductListview extends StatelessWidget {
@@ -16,55 +17,63 @@ class ProductListview extends StatelessWidget {
     return ListView.builder(
       itemCount: products?.products?.length,
       itemBuilder: (context, index) {
-        return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Text(
-                      "${products?.products?[index].name}",
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Image.network(
-                                ("https://assignment-api.piton.com.tr${products!.products?[index].image}")),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Text(
-                              "${products?.products?[index].description}",
+        return InkWell(
+          onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    ProductDetail(product: products?.products?[index]),
+              )),
+          child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "${products?.products?[index].name}",
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.network(
+                                  ("https://assignment-api.piton.com.tr${products!.products?[index].image}")),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    IconButton(
-                      icon: Icon(products!.favCount!
-                              .contains(products!.products![index].id)
-                          ? Icons.favorite
-                          : Icons.favorite_border),
-                      onPressed: () => products!.favCount!
-                              .contains(products!.products![index].id)
-                          ? context
-                              .read<ProductProvider>()
-                              .unlikeProduct(products!.products?[index].id)
-                          : context
-                              .read<ProductProvider>()
-                              .likeProduct(products!.products?[index].id),
-                    )
-                  ],
+                          Expanded(
+                            flex: 2,
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Text(
+                                "${products?.products?[index].description}",
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        icon: Icon(products!.favCount!
+                                .contains(products!.products![index].id)
+                            ? Icons.favorite
+                            : Icons.favorite_border),
+                        onPressed: () => products!.favCount!
+                                .contains(products!.products![index].id)
+                            ? context
+                                .read<ProductProvider>()
+                                .unlikeProduct(products!.products?[index].id)
+                            : context
+                                .read<ProductProvider>()
+                                .likeProduct(products!.products?[index].id),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ));
+              )),
+        );
       },
     );
   }
